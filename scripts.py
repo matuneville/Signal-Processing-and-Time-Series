c78fcd1c3d27deee3a83061b92aa6b5d99ede198
matuneville <nevillematias@gmail.com>
2025-03-27 14:48:50 -0300

    fix

--- scripts.py
+++ scripts.py
@@ -20,14 +20,14 @@ def plot_time_series(x, y, title="", x_label="Time", y_label="Values"):
 
     fig.show()
 
-def plot_time_series_multiple_y(x, y_dict, title="", x_label="Time", y_label="Values"):
+def plot_time_series_multiple_y(x, y_dict, title="", x_label="Time", y_label="Values", markers=True):
     fig = go.Figure()
 
     for label, y_values in y_dict.items():
         fig.add_trace(go.Scatter(
             x=x,
             y=y_values,
-            mode='lines+markers',
+            mode=f'lines{'+markers' if markers else ''}',
             name=label,  # Use the dictionary key as the series name
             hovertemplate=f'{x_label}: %{{x}}<br>{label}: %{{y}}'
         ))
@@ -40,4 +40,40 @@ def plot_time_series_multiple_y(x, y_dict, title="", x_label="Time", y_label="Va
         legend_title="Legend"
     )
 
+    fig.show()
+
+
+def plot_boxplots(x, y, title="", x_label="Time", y_label="Values"):
+    """
+    Function to plot boxplots using Plotly for average temperatures across multiple years.
+
+    Parameters:
+    x (list): List of months to be displayed on the x-axis (e.g., ['Jan', 'Feb', ...])
+    y (list): List of temperature data, where each element is a list of temperatures for a given month across years.
+    """
+    # Create the boxplot data
+    boxplot_data = []
+
+    # Populate the boxplot data for each month
+    for i, month in enumerate(x):
+        boxplot_data.append(go.Box(
+            y=y[i],
+            name=month,
+            boxmean='sd',  # Show the standard deviation in the box plot (can be adjusted to other options)
+            #jitter=0.5,  # Adds jitter to separate the points a little
+            pointpos=0,  # Controls the position of individual points
+            width=0.6,
+        ))
+
+    # Create the layout for the plot
+    layout = go.Layout(
+        title=title,
+        xaxis=dict(title=x_label),
+        yaxis=dict(title=y_label),
+        boxmode='group',  # To group the boxplots by month
+        height=1000,
+    )
+
+    # Create the figure and display it
+    fig = go.Figure(data=boxplot_data, layout=layout)
     fig.show()
